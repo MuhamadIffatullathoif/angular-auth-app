51aacd0c8a74c4f5ead37e0986fb317e5d43d6cf
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-27 06:52:11 +0700

    03. Login Form

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  private fb = inject(FormBuilder);
 
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  login(): void {
+    console.log(this.myForm.getRawValue());
+  }
 }
